919acfd4f519021096c788f0de27dee56f4e089d
BhavaniKamadi <bhavani@spondias.com>
2025-09-22 22:23:28 +0530

    changes

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URL, {
-      dbName: "phone",
+      dbName: "phonenumberverification",
     });
 
     return connection;
